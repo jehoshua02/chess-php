3b4cd8580d7b4c2f3896d7724a1da10738656802
Joshua Stoutenburg <jstoutenburg@deseretdigital.com>
2013-10-18 13:46:36 -0600

    Renames Pieces to be More Presentation Agnostic

--- src/Chess/Board.php
+++ src/Chess/Board.php
@@ -4,61 +4,61 @@ namespace Chess;
 
 class Board
 {
-    const WHITE_KING = 'WHITE_KING';
-    const WHITE_QUEEN = 'WHITE_QUEEN';
-    const WHITE_BISHOP = 'WHITE_BISHOP';
-    const WHITE_KNIGHT = 'WHITE_KNIGHT';
-    const WHITE_ROOK = 'WHITE_ROOK';
-    const WHITE_PAWN = 'WHITE_PAWN';
+    const LIGHT_KING = 'LIGHT_KING';
+    const LIGHT_QUEEN = 'LIGHT_QUEEN';
+    const LIGHT_BISHOP = 'LIGHT_BISHOP';
+    const LIGHT_KNIGHT = 'LIGHT_KNIGHT';
+    const LIGHT_ROOK = 'LIGHT_ROOK';
+    const LIGHT_PAWN = 'LIGHT_PAWN';
 
-    const BLACK_KING = 'BLACK_KING';
-    const BLACK_QUEEN = 'BLACK_QUEEN';
-    const BLACK_BISHOP = 'BLACK_BISHOP';
-    const BLACK_KNIGHT = 'BLACK_KNIGHT';
-    const BLACK_ROOK = 'BLACK_ROOK';
-    const BLACK_PAWN = 'BLACK_PAWN';
+    const DARK_KING = 'DARK_KING';
+    const DARK_QUEEN = 'DARK_QUEEN';
+    const DARK_BISHOP = 'DARK_BISHOP';
+    const DARK_KNIGHT = 'DARK_KNIGHT';
+    const DARK_ROOK = 'DARK_ROOK';
+    const DARK_PAWN = 'DARK_PAWN';
 
     protected $positions = array(
-        'A1' => self::WHITE_ROOK,
-        'B1' => self::WHITE_KNIGHT,
-        'C1' => self::WHITE_BISHOP,
-        'D1' => self::WHITE_QUEEN,
-        'E1' => self::WHITE_KING,
-        'F1' => self::WHITE_BISHOP,
-        'G1' => self::WHITE_KNIGHT,
-        'H1' => self::WHITE_ROOK,
+        'A1' => self::LIGHT_ROOK,
+        'B1' => self::LIGHT_KNIGHT,
+        'C1' => self::LIGHT_BISHOP,
+        'D1' => self::LIGHT_QUEEN,
+        'E1' => self::LIGHT_KING,
+        'F1' => self::LIGHT_BISHOP,
+        'G1' => self::LIGHT_KNIGHT,
+        'H1' => self::LIGHT_ROOK,
 
-        'A2' => self::WHITE_PAWN,
-        'B2' => self::WHITE_PAWN,
-        'C2' => self::WHITE_PAWN,
-        'D2' => self::WHITE_PAWN,
-        'E2' => self::WHITE_PAWN,
-        'F2' => self::WHITE_PAWN,
-        'G2' => self::WHITE_PAWN,
-        'H2' => self::WHITE_PAWN,
+        'A2' => self::LIGHT_PAWN,
+        'B2' => self::LIGHT_PAWN,
+        'C2' => self::LIGHT_PAWN,
+        'D2' => self::LIGHT_PAWN,
+        'E2' => self::LIGHT_PAWN,
+        'F2' => self::LIGHT_PAWN,
+        'G2' => self::LIGHT_PAWN,
+        'H2' => self::LIGHT_PAWN,
 
-        'A7' => self::BLACK_PAWN,
-        'B7' => self::BLACK_PAWN,
-        'C7' => self::BLACK_PAWN,
-        'D7' => self::BLACK_PAWN,
-        'E7' => self::BLACK_PAWN,
-        'F7' => self::BLACK_PAWN,
-        'G7' => self::BLACK_PAWN,
-        'H7' => self::BLACK_PAWN,
+        'A7' => self::DARK_PAWN,
+        'B7' => self::DARK_PAWN,
+        'C7' => self::DARK_PAWN,
+        'D7' => self::DARK_PAWN,
+        'E7' => self::DARK_PAWN,
+        'F7' => self::DARK_PAWN,
+        'G7' => self::DARK_PAWN,
+        'H7' => self::DARK_PAWN,
 
-        'A8' => self::BLACK_ROOK,
-        'B8' => self::BLACK_KNIGHT,
-        'C8' => self::BLACK_BISHOP,
-        'D8' => self::BLACK_KING,
-        'E8' => self::BLACK_QUEEN,
-        'F8' => self::BLACK_BISHOP,
-        'G8' => self::BLACK_KNIGHT,
-        'H8' => self::BLACK_ROOK,
+        'A8' => self::DARK_ROOK,
+        'B8' => self::DARK_KNIGHT,
+        'C8' => self::DARK_BISHOP,
+        'D8' => self::DARK_KING,
+        'E8' => self::DARK_QUEEN,
+        'F8' => self::DARK_BISHOP,
+        'G8' => self::DARK_KNIGHT,
+        'H8' => self::DARK_ROOK,
     );
 
     /**
      * Construct method
-     * @param array|null $positions Array of positions, where key is algebraic notation (A1 through H8), and value is the piece (Board::WHITE_PAWN)
+     * @param array|null $positions Array of positions, where key is algebraic notation (A1 through H8), and value is the piece (Board::LIGHT_PAWN)
      */
     public function __construct(array $positions = null)
     {
--- tests/Chess/BoardTest.php
+++ tests/Chess/BoardTest.php
@@ -12,41 +12,41 @@ class BoardTest extends \PHPUnit_Framework_TestCase
     {
         $board = new Board();
 
-        $this->assertTrue($board->pieceAt('A1') === Board::WHITE_ROOK, 'A1 should be WHITE_ROOK');
-        $this->assertTrue($board->pieceAt('B1') === Board::WHITE_KNIGHT, 'B1 should be WHITE_KNIGHT');
-        $this->assertTrue($board->pieceAt('C1') === Board::WHITE_BISHOP, 'C1 should be WHITE_BISHOP');
-        $this->assertTrue($board->pieceAt('D1') === Board::WHITE_QUEEN, 'D1 should be WHITE_QUEEN');
-        $this->assertTrue($board->pieceAt('E1') === Board::WHITE_KING, 'E1 should be WHITE_KING');
-        $this->assertTrue($board->pieceAt('F1') === Board::WHITE_BISHOP, 'F1 should be WHITE_BISHOP');
-        $this->assertTrue($board->pieceAt('G1') === Board::WHITE_KNIGHT, 'G1 should be WHITE_KNIGHT');
-        $this->assertTrue($board->pieceAt('H1') === Board::WHITE_ROOK, 'H1 should be WHITE_ROOK');
+        $this->assertTrue($board->pieceAt('A1') === Board::LIGHT_ROOK, 'A1 should be LIGHT_ROOK');
+        $this->assertTrue($board->pieceAt('B1') === Board::LIGHT_KNIGHT, 'B1 should be LIGHT_KNIGHT');
+        $this->assertTrue($board->pieceAt('C1') === Board::LIGHT_BISHOP, 'C1 should be LIGHT_BISHOP');
+        $this->assertTrue($board->pieceAt('D1') === Board::LIGHT_QUEEN, 'D1 should be LIGHT_QUEEN');
+        $this->assertTrue($board->pieceAt('E1') === Board::LIGHT_KING, 'E1 should be LIGHT_KING');
+        $this->assertTrue($board->pieceAt('F1') === Board::LIGHT_BISHOP, 'F1 should be LIGHT_BISHOP');
+        $this->assertTrue($board->pieceAt('G1') === Board::LIGHT_KNIGHT, 'G1 should be LIGHT_KNIGHT');
+        $this->assertTrue($board->pieceAt('H1') === Board::LIGHT_ROOK, 'H1 should be LIGHT_ROOK');
 
-        $this->assertTrue($board->pieceAt('A2') === Board::WHITE_PAWN, 'A2 should be WHITE_PAWN');
-        $this->assertTrue($board->pieceAt('B2') === Board::WHITE_PAWN, 'B2 should be WHITE_PAWN');
-        $this->assertTrue($board->pieceAt('C2') === Board::WHITE_PAWN, 'C2 should be WHITE_PAWN');
-        $this->assertTrue($board->pieceAt('D2') === Board::WHITE_PAWN, 'D2 should be WHITE_PAWN');
-        $this->assertTrue($board->pieceAt('E2') === Board::WHITE_PAWN, 'E2 should be WHITE_PAWN');
-        $this->assertTrue($board->pieceAt('F2') === Board::WHITE_PAWN, 'F2 should be WHITE_PAWN');
-        $this->assertTrue($board->pieceAt('G2') === Board::WHITE_PAWN, 'G2 should be WHITE_PAWN');
-        $this->assertTrue($board->pieceAt('H2') === Board::WHITE_PAWN, 'H2 should be WHITE_PAWN');
+        $this->assertTrue($board->pieceAt('A2') === Board::LIGHT_PAWN, 'A2 should be LIGHT_PAWN');
+        $this->assertTrue($board->pieceAt('B2') === Board::LIGHT_PAWN, 'B2 should be LIGHT_PAWN');
+        $this->assertTrue($board->pieceAt('C2') === Board::LIGHT_PAWN, 'C2 should be LIGHT_PAWN');
+        $this->assertTrue($board->pieceAt('D2') === Board::LIGHT_PAWN, 'D2 should be LIGHT_PAWN');
+        $this->assertTrue($board->pieceAt('E2') === Board::LIGHT_PAWN, 'E2 should be LIGHT_PAWN');
+        $this->assertTrue($board->pieceAt('F2') === Board::LIGHT_PAWN, 'F2 should be LIGHT_PAWN');
+        $this->assertTrue($board->pieceAt('G2') === Board::LIGHT_PAWN, 'G2 should be LIGHT_PAWN');
+        $this->assertTrue($board->pieceAt('H2') === Board::LIGHT_PAWN, 'H2 should be LIGHT_PAWN');
 
-        $this->assertTrue($board->pieceAt('A7') === Board::BLACK_PAWN, 'A7 should be BLACK_PAWN');
-        $this->assertTrue($board->pieceAt('B7') === Board::BLACK_PAWN, 'B7 should be BLACK_PAWN');
-        $this->assertTrue($board->pieceAt('C7') === Board::BLACK_PAWN, 'C7 should be BLACK_PAWN');
-        $this->assertTrue($board->pieceAt('D7') === Board::BLACK_PAWN, 'D7 should be BLACK_PAWN');
-        $this->assertTrue($board->pieceAt('E7') === Board::BLACK_PAWN, 'E7 should be BLACK_PAWN');
-        $this->assertTrue($board->pieceAt('F7') === Board::BLACK_PAWN, 'F7 should be BLACK_PAWN');
-        $this->assertTrue($board->pieceAt('G7') === Board::BLACK_PAWN, 'G7 should be BLACK_PAWN');
-        $this->assertTrue($board->pieceAt('H7') === Board::BLACK_PAWN, 'H7 should be BLACK_PAWN');
+        $this->assertTrue($board->pieceAt('A7') === Board::DARK_PAWN, 'A7 should be DARK_PAWN');
+        $this->assertTrue($board->pieceAt('B7') === Board::DARK_PAWN, 'B7 should be DARK_PAWN');
+        $this->assertTrue($board->pieceAt('C7') === Board::DARK_PAWN, 'C7 should be DARK_PAWN');
+        $this->assertTrue($board->pieceAt('D7') === Board::DARK_PAWN, 'D7 should be DARK_PAWN');
+        $this->assertTrue($board->pieceAt('E7') === Board::DARK_PAWN, 'E7 should be DARK_PAWN');
+        $this->assertTrue($board->pieceAt('F7') === Board::DARK_PAWN, 'F7 should be DARK_PAWN');
+        $this->assertTrue($board->pieceAt('G7') === Board::DARK_PAWN, 'G7 should be DARK_PAWN');
+        $this->assertTrue($board->pieceAt('H7') === Board::DARK_PAWN, 'H7 should be DARK_PAWN');
 
-        $this->assertTrue($board->pieceAt('A8') === Board::BLACK_ROOK, 'A8 should be BLACK_ROOK');
-        $this->assertTrue($board->pieceAt('B8') === Board::BLACK_KNIGHT, 'B8 should be BLACK_KNIGHT');
-        $this->assertTrue($board->pieceAt('C8') === Board::BLACK_BISHOP, 'C8 should be BLACK_BISHOP');
-        $this->assertTrue($board->pieceAt('D8') === Board::BLACK_KING, 'D8 should be BLACK_KING');
-        $this->assertTrue($board->pieceAt('E8') === Board::BLACK_QUEEN, 'E8 should be BLACK_QUEEN');
-        $this->assertTrue($board->pieceAt('F8') === Board::BLACK_BISHOP, 'F8 should be BLACK_BISHOP');
-        $this->assertTrue($board->pieceAt('G8') === Board::BLACK_KNIGHT, 'G8 should be BLACK_KNIGHT');
-        $this->assertTrue($board->pieceAt('H8') === Board::BLACK_ROOK, 'H8 should be BLACK_ROOK');
+        $this->assertTrue($board->pieceAt('A8') === Board::DARK_ROOK, 'A8 should be DARK_ROOK');
+        $this->assertTrue($board->pieceAt('B8') === Board::DARK_KNIGHT, 'B8 should be DARK_KNIGHT');
+        $this->assertTrue($board->pieceAt('C8') === Board::DARK_BISHOP, 'C8 should be DARK_BISHOP');
+        $this->assertTrue($board->pieceAt('D8') === Board::DARK_KING, 'D8 should be DARK_KING');
+        $this->assertTrue($board->pieceAt('E8') === Board::DARK_QUEEN, 'E8 should be DARK_QUEEN');
+        $this->assertTrue($board->pieceAt('F8') === Board::DARK_BISHOP, 'F8 should be DARK_BISHOP');
+        $this->assertTrue($board->pieceAt('G8') === Board::DARK_KNIGHT, 'G8 should be DARK_KNIGHT');
+        $this->assertTrue($board->pieceAt('H8') === Board::DARK_ROOK, 'H8 should be DARK_ROOK');
 
     }
 
@@ -74,10 +74,10 @@ class BoardTest extends \PHPUnit_Framework_TestCase
     public function testCustomBoard()
     {
         $board = new Board(array(
-            'A1' => Board::WHITE_PAWN,
+            'A1' => Board::LIGHT_PAWN
         ));
 
-        $this->assertTrue($board->pieceAt('A1') === Board::WHITE_PAWN);
+        $this->assertTrue($board->pieceAt('A1') === Board::LIGHT_PAWN);
         $this->assertFalse($board->pieceAt('B1'));
     }
 }
