b47aff55422465fefc02aa8537d264e06bfd0eec
Joshua Stoutenburg <jstoutenburg@deseretdigital.com>
2013-10-18 18:51:25 -0600

    Better Utilization of PHPUnit Assertions

--- tests/Chess/BoardTest.php
+++ tests/Chess/BoardTest.php
@@ -12,41 +12,41 @@ class BoardTest extends \PHPUnit_Framework_TestCase
     {
         $board = new Board();
 
-        $this->assertTrue($board->pieceAt('A1') === Board::LIGHT_ROOK, 'A1 should be LIGHT_ROOK');
-        $this->assertTrue($board->pieceAt('B1') === Board::LIGHT_KNIGHT, 'B1 should be LIGHT_KNIGHT');
-        $this->assertTrue($board->pieceAt('C1') === Board::LIGHT_BISHOP, 'C1 should be LIGHT_BISHOP');
-        $this->assertTrue($board->pieceAt('D1') === Board::LIGHT_QUEEN, 'D1 should be LIGHT_QUEEN');
-        $this->assertTrue($board->pieceAt('E1') === Board::LIGHT_KING, 'E1 should be LIGHT_KING');
-        $this->assertTrue($board->pieceAt('F1') === Board::LIGHT_BISHOP, 'F1 should be LIGHT_BISHOP');
-        $this->assertTrue($board->pieceAt('G1') === Board::LIGHT_KNIGHT, 'G1 should be LIGHT_KNIGHT');
-        $this->assertTrue($board->pieceAt('H1') === Board::LIGHT_ROOK, 'H1 should be LIGHT_ROOK');
+        $this->assertEquals($board->pieceAt('A1'), Board::LIGHT_ROOK, 'A1 should be LIGHT_ROOK');
+        $this->assertEquals($board->pieceAt('B1'), Board::LIGHT_KNIGHT, 'B1 should be LIGHT_KNIGHT');
+        $this->assertEquals($board->pieceAt('C1'), Board::LIGHT_BISHOP, 'C1 should be LIGHT_BISHOP');
+        $this->assertEquals($board->pieceAt('D1'), Board::LIGHT_QUEEN, 'D1 should be LIGHT_QUEEN');
+        $this->assertEquals($board->pieceAt('E1'), Board::LIGHT_KING, 'E1 should be LIGHT_KING');
+        $this->assertEquals($board->pieceAt('F1'), Board::LIGHT_BISHOP, 'F1 should be LIGHT_BISHOP');
+        $this->assertEquals($board->pieceAt('G1'), Board::LIGHT_KNIGHT, 'G1 should be LIGHT_KNIGHT');
+        $this->assertEquals($board->pieceAt('H1'), Board::LIGHT_ROOK, 'H1 should be LIGHT_ROOK');
 
-        $this->assertTrue($board->pieceAt('A2') === Board::LIGHT_PAWN, 'A2 should be LIGHT_PAWN');
-        $this->assertTrue($board->pieceAt('B2') === Board::LIGHT_PAWN, 'B2 should be LIGHT_PAWN');
-        $this->assertTrue($board->pieceAt('C2') === Board::LIGHT_PAWN, 'C2 should be LIGHT_PAWN');
-        $this->assertTrue($board->pieceAt('D2') === Board::LIGHT_PAWN, 'D2 should be LIGHT_PAWN');
-        $this->assertTrue($board->pieceAt('E2') === Board::LIGHT_PAWN, 'E2 should be LIGHT_PAWN');
-        $this->assertTrue($board->pieceAt('F2') === Board::LIGHT_PAWN, 'F2 should be LIGHT_PAWN');
-        $this->assertTrue($board->pieceAt('G2') === Board::LIGHT_PAWN, 'G2 should be LIGHT_PAWN');
-        $this->assertTrue($board->pieceAt('H2') === Board::LIGHT_PAWN, 'H2 should be LIGHT_PAWN');
+        $this->assertEquals($board->pieceAt('A2'), Board::LIGHT_PAWN, 'A2 should be LIGHT_PAWN');
+        $this->assertEquals($board->pieceAt('B2'), Board::LIGHT_PAWN, 'B2 should be LIGHT_PAWN');
+        $this->assertEquals($board->pieceAt('C2'), Board::LIGHT_PAWN, 'C2 should be LIGHT_PAWN');
+        $this->assertEquals($board->pieceAt('D2'), Board::LIGHT_PAWN, 'D2 should be LIGHT_PAWN');
+        $this->assertEquals($board->pieceAt('E2'), Board::LIGHT_PAWN, 'E2 should be LIGHT_PAWN');
+        $this->assertEquals($board->pieceAt('F2'), Board::LIGHT_PAWN, 'F2 should be LIGHT_PAWN');
+        $this->assertEquals($board->pieceAt('G2'), Board::LIGHT_PAWN, 'G2 should be LIGHT_PAWN');
+        $this->assertEquals($board->pieceAt('H2'), Board::LIGHT_PAWN, 'H2 should be LIGHT_PAWN');
 
-        $this->assertTrue($board->pieceAt('A7') === Board::DARK_PAWN, 'A7 should be DARK_PAWN');
-        $this->assertTrue($board->pieceAt('B7') === Board::DARK_PAWN, 'B7 should be DARK_PAWN');
-        $this->assertTrue($board->pieceAt('C7') === Board::DARK_PAWN, 'C7 should be DARK_PAWN');
-        $this->assertTrue($board->pieceAt('D7') === Board::DARK_PAWN, 'D7 should be DARK_PAWN');
-        $this->assertTrue($board->pieceAt('E7') === Board::DARK_PAWN, 'E7 should be DARK_PAWN');
-        $this->assertTrue($board->pieceAt('F7') === Board::DARK_PAWN, 'F7 should be DARK_PAWN');
-        $this->assertTrue($board->pieceAt('G7') === Board::DARK_PAWN, 'G7 should be DARK_PAWN');
-        $this->assertTrue($board->pieceAt('H7') === Board::DARK_PAWN, 'H7 should be DARK_PAWN');
+        $this->assertEquals($board->pieceAt('A7'), Board::DARK_PAWN, 'A7 should be DARK_PAWN');
+        $this->assertEquals($board->pieceAt('B7'), Board::DARK_PAWN, 'B7 should be DARK_PAWN');
+        $this->assertEquals($board->pieceAt('C7'), Board::DARK_PAWN, 'C7 should be DARK_PAWN');
+        $this->assertEquals($board->pieceAt('D7'), Board::DARK_PAWN, 'D7 should be DARK_PAWN');
+        $this->assertEquals($board->pieceAt('E7'), Board::DARK_PAWN, 'E7 should be DARK_PAWN');
+        $this->assertEquals($board->pieceAt('F7'), Board::DARK_PAWN, 'F7 should be DARK_PAWN');
+        $this->assertEquals($board->pieceAt('G7'), Board::DARK_PAWN, 'G7 should be DARK_PAWN');
+        $this->assertEquals($board->pieceAt('H7'), Board::DARK_PAWN, 'H7 should be DARK_PAWN');
 
-        $this->assertTrue($board->pieceAt('A8') === Board::DARK_ROOK, 'A8 should be DARK_ROOK');
-        $this->assertTrue($board->pieceAt('B8') === Board::DARK_KNIGHT, 'B8 should be DARK_KNIGHT');
-        $this->assertTrue($board->pieceAt('C8') === Board::DARK_BISHOP, 'C8 should be DARK_BISHOP');
-        $this->assertTrue($board->pieceAt('D8') === Board::DARK_KING, 'D8 should be DARK_KING');
-        $this->assertTrue($board->pieceAt('E8') === Board::DARK_QUEEN, 'E8 should be DARK_QUEEN');
-        $this->assertTrue($board->pieceAt('F8') === Board::DARK_BISHOP, 'F8 should be DARK_BISHOP');
-        $this->assertTrue($board->pieceAt('G8') === Board::DARK_KNIGHT, 'G8 should be DARK_KNIGHT');
-        $this->assertTrue($board->pieceAt('H8') === Board::DARK_ROOK, 'H8 should be DARK_ROOK');
+        $this->assertEquals($board->pieceAt('A8'), Board::DARK_ROOK, 'A8 should be DARK_ROOK');
+        $this->assertEquals($board->pieceAt('B8'), Board::DARK_KNIGHT, 'B8 should be DARK_KNIGHT');
+        $this->assertEquals($board->pieceAt('C8'), Board::DARK_BISHOP, 'C8 should be DARK_BISHOP');
+        $this->assertEquals($board->pieceAt('D8'), Board::DARK_KING, 'D8 should be DARK_KING');
+        $this->assertEquals($board->pieceAt('E8'), Board::DARK_QUEEN, 'E8 should be DARK_QUEEN');
+        $this->assertEquals($board->pieceAt('F8'), Board::DARK_BISHOP, 'F8 should be DARK_BISHOP');
+        $this->assertEquals($board->pieceAt('G8'), Board::DARK_KNIGHT, 'G8 should be DARK_KNIGHT');
+        $this->assertEquals($board->pieceAt('H8'), Board::DARK_ROOK, 'H8 should be DARK_ROOK');
 
     }
 
@@ -77,8 +77,8 @@ class BoardTest extends \PHPUnit_Framework_TestCase
             'A1' => Board::LIGHT_PAWN
         ));
 
-        $this->assertTrue($board->pieceAt('A1') === Board::LIGHT_PAWN);
-        $this->assertFalse($board->pieceAt('B1'));
+        $this->assertEquals($board->pieceAt('A1'), Board::LIGHT_PAWN, 'A1 should be LIGHT_PAWN');
+        $this->assertFalse($board->pieceAt('B1'), 'B1 should be empty');
     }
 
     /**
@@ -91,8 +91,8 @@ class BoardTest extends \PHPUnit_Framework_TestCase
             'D4' => Board::LIGHT_PAWN,
         ));
         $moves = $board->movesFor('D4');
-        $this->assertCount(1, $moves, 'Pawn only has one possible move');
-        $this->assertContains('D5', $moves, 'Pawn can move forward');
+        $this->assertCount(1, $moves, 'Pawn should only have one possible move');
+        $this->assertContains('D5', $moves, 'Pawn should be able to move one forward');
 
         // @todo edge of board / promotion
         // @todo blocked
