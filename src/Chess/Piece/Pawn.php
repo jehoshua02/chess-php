957271e6ae0c7f9ad922da10f6403e1145a5197c
Joshua Stoutenburg <jstoutenburg@deseretdigital.com>
2013-10-22 22:26:24 -0600

    Implements Pawn->up

--- src/Chess/Piece/Pawn.php
+++ src/Chess/Piece/Pawn.php
@@ -4,5 +4,13 @@ namespace Chess\Piece;
 
 class Pawn extends \Chess\Piece
 {
-
+    /**
+     * Returns position above piece's position
+     * @return string|false Returns false if not valid move for piece
+     */
+    public function up()
+    {
+        $up = $this->board->up($this->getPosition());
+        return $this->board->get($up) ? false : $up;
+    }
 }
