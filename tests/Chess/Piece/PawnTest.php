e147ec30fe84bc5995a62e47d2948cf703ea9250
Joshua Stoutenburg <jstoutenburg@deseretdigital.com>
2013-10-23 00:55:24 -0600

    Adds assertion to PawnTest::testUpLeft
    Asserts that pawn can capture up and left

--- tests/Chess/Piece/PawnTest.php
+++ tests/Chess/Piece/PawnTest.php
@@ -75,5 +75,11 @@ class PawnTest extends \PHPUnit_Framework_TestCase
             'C5' => new Pawn(Piece::LIGHT)
         ));
         $this->assertFalse($board->get('D4')->upLeft(), 'Pawn should not be able to capture own color');
+
+        $board = new Board(array(
+            'D4' => new Pawn(Piece::LIGHT),
+            'C5' => new Pawn(Piece::DARK)
+        ));
+        $this->assertEquals('C5', $board->get('D4')->upLeft(), 'Pawn should be able to capture up and left');
     }
 }
