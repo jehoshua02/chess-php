00e14b0b36db534b4bd00e940a84401ab5406dbe
Joshua Stoutenburg <jstoutenburg@deseretdigital.com>
2013-10-22 22:55:39 -0600

    Adds assertion for blocked pawn

--- tests/Chess/Piece/PawnTest.php
+++ tests/Chess/Piece/PawnTest.php
@@ -13,5 +13,12 @@ class PawnTest extends \PHPUnit_Framework_TestCase
             'D4' => new Pawn(Piece::LIGHT)
         ));
         $this->assertEquals('D5', $board->get('D4')->up(), 'Pawn should be able to move up');
+
+        // blocked
+        $board = new Board(array(
+            'D4' => new Pawn(Piece::LIGHT),
+            'D5' => new Pawn(Piece::DARK)
+        ));
+        $this->assertFalse($board->get('D4')->up(), 'Pawn should not be able to move up');
     }
 }
