c291312f39444205e6305719550cbd72359b292b
Joshua Stoutenburg <jstoutenburg@deseretdigital.com>
2013-10-28 21:25:19 -0600

    Satisfies KingTest::testNoMoves

--- src/Chess/Piece/King.php
+++ src/Chess/Piece/King.php
@@ -4,5 +4,12 @@ namespace Chess\Piece;
 
 class King extends \Chess\Piece
 {
-
+    /**
+     * Returns all possible moves
+     * @return array
+     */
+    public function moves()
+    {
+        return array();
+    }
 }
