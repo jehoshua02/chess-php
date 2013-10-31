8ad33788a5c930714eb08d5b927307ff9e7aa31c
Joshua Stoutenburg <jstoutenburg@deseretdigital.com>
2013-10-30 23:04:00 -0600

    Adds KnightTest::testMovesBlocked

--- tests/Chess/Piece/KnightTest.php
+++ tests/Chess/Piece/KnightTest.php
@@ -21,4 +21,29 @@ class KnightTest extends \PHPUnit_Framework_TestCase
             $this->assertContains($position, $moves, sprintf($message, $position));
         }
     }
+
+    public function testMovesBlocked()
+    {
+        $board = new Board(array(
+            'D4' => new Knight(Piece::LIGHT),
+
+            // pieces to jump over to get to C6
+            'C5' => new Pawn(Piece::LIGHT),
+            'D6' => new Pawn(Piece::LIGHT),
+
+            // piece to capture in E6
+            'E6' => new Pawn(Piece::DARK),
+
+            // same color will block E2
+            'E2' => new Pawn(Piece::LIGHT)
+        ));
+        $moves = $board->piece('D4')->moves();
+        $this->assertCount(7, $moves, 'Knight should have eight possible moves');
+
+        $message = 'Knight should be able to move to %s';
+        $positions = array('C6', 'E6', 'C2', 'B3', 'B5', 'F3', 'F5');
+        foreach ($positions as $position) {
+            $this->assertContains($position, $moves, sprintf($message, $position));
+        }
+    }
 }
