918a9b6d7f544dfe5368e01c3a0ff471d3845f96
Joshua Stoutenburg <jstoutenburg@deseretdigital.com>
2013-10-30 22:57:48 -0600

    Satisfies KnightTest::testMoves

--- src/Chess/Piece/Knight.php
+++ src/Chess/Piece/Knight.php
@@ -4,5 +4,51 @@ namespace Chess\Piece;
 
 class Knight extends \Chess\Piece
 {
+    /**
+     * Returns all possible moves
+     * @return array
+     */
+    public function moves()
+    {
+        $moves = array(
+            $this->jump('up', 'upLeft'),
+            $this->jump('up', 'upRight'),
+            $this->jump('down', 'downLeft'),
+            $this->jump('down', 'downRight'),
+            $this->jump('left', 'upLeft'),
+            $this->jump('left', 'downLeft'),
+            $this->jump('right', 'upRight'),
+            $this->jump('right', 'downRight')
+        );
 
+        $moves = array_filter($moves, function ($move) {
+            return $move !== false;
+        });
+
+        return $moves;
+    }
+
+    /**
+     * Returns jump position
+     * @param  string $direction, $direction, ...
+     * @return string|false Returns false if not a valid move for piece
+     */
+    protected function jump()
+    {
+        $directions = func_get_args();
+        $position = $this->position();
+        foreach ($directions as $direction) {
+            $position = $this->board()->$direction($position);
+            if (!$position) {
+                return false;
+            }
+        }
+
+        $piece = $this->board()->piece($position);
+        if ($piece && $piece->color() === $this->color()) {
+            return false;
+        }
+
+        return $position;
+    }
 }
