a028a38199cdfcb0b17d8dd5542de8440e613b6a
Joshua Stoutenburg <jstoutenburg@deseretdigital.com>
2013-10-30 21:53:48 -0600

    Satisfies QueenTest::testMoves

--- src/Chess/Piece/Queen.php
+++ src/Chess/Piece/Queen.php
@@ -4,5 +4,22 @@ namespace Chess\Piece;
 
 class Queen extends \Chess\Piece
 {
-
+    /**
+     * Returns all possible moves
+     * @return array
+     */
+    public function moves()
+    {
+        $moves = array();
+        $directions = array(
+            // same as Rook
+            'up', 'down', 'left', 'right',
+            // plus the same as Bishop
+            'upLeft', 'upRight', 'downLeft', 'downRight'
+        );
+        foreach ($directions as $direction) {
+            $moves = array_merge($moves, $this->slide($direction));
+        }
+        return $moves;
+    }
 }
