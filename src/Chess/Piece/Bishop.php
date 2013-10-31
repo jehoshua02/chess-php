d432ba87cce495331dbe537e9817fd0f93bb5e39
Joshua Stoutenburg <jstoutenburg@deseretdigital.com>
2013-10-30 21:17:37 -0600

    Satisfies BishopTest::testMoves
    + Bishop should have 13 possible moves
    + Bishop should be able to move to %s

--- src/Chess/Piece/Bishop.php
+++ src/Chess/Piece/Bishop.php
@@ -4,5 +4,39 @@ namespace Chess\Piece;
 
 class Bishop extends \Chess\Piece
 {
+    /**
+     * Returns all possible moves
+     * @return array
+     */
+    public function moves()
+    {
+        $moves = array();
+        foreach (array('upLeft', 'upRight', 'downLeft', 'downRight') as $direction) {
+            $moves = array_merge($moves, $this->slide($direction));
+        }
+        return $moves;
+    }
 
+    /**
+     * Returns all moves in one direction
+     * @param  string $direction
+     * @return array
+     */
+    protected function slide($direction)
+    {
+        $moves = array();
+
+        $position = $this->position();
+        while (true) {
+            $position = $this->board()->$direction($position);
+
+            if (!$position) {
+                break;
+            }
+
+            $moves[] = $position;
+        }
+
+        return $moves;
+    }
 }
--- tests/Chess/Piece/BishopTest.php
+++ tests/Chess/Piece/BishopTest.php
@@ -12,27 +12,12 @@ class BishopTest extends \PHPUnit_Framework_TestCase
             'D4' => new Bishop(Piece::LIGHT)
         ));
         $moves = $board->piece('D4')->moves();
-        $this->assertCount(13, $moves);
+        $this->assertCount(13, $moves, 'Bishop should have 13 possible moves');
 
-        // down left
-        $this->assertContains('A1', $moves, 'Bishop should be able to move to A1');
-        $this->assertContains('B2', $moves, 'Bishop should be able to move to B2');
-        $this->assertContains('C3', $moves, 'Bishop should be able to move to C3');
-
-        // up right
-        $this->assertContains('E5', $moves, 'Bishop should be able to move to E5');
-        $this->assertContains('F6', $moves, 'Bishop should be able to move to F6');
-        $this->assertContains('G7', $moves, 'Bishop should be able to move to G7');
-        $this->assertContains('H8', $moves, 'Bishop should be able to move to H8');
-
-        // up left
-        $this->assertContains('A7', $moves, 'Bishop should be able to move to A7');
-        $this->assertContains('B6', $moves, 'Bishop should be able to move to B6');
-        $this->assertContains('C5', $moves, 'Bishop should be able to move to C5');
-
-        // down right
-        $this->assertContains('E3', $moves, 'Bishop should be able to move to E3');
-        $this->assertContains('F2', $moves, 'Bishop should be able to move to F2');
-        $this->assertContains('G1', $moves, 'Bishop should be able to move to G1');
+        $message = 'Bishop should be able to move to %s';
+        $positions = array('A1', 'B2', 'C3', 'E5', 'F6', 'G7', 'H8', 'A7', 'B6', 'C5', 'E3', 'F2', 'G1');
+        foreach ($positions as $position) {
+            $this->assertContains($position, $moves, sprintf($message, $position));
+        }
     }
 }
