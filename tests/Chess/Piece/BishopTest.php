a640d47928992386cc94585af2d1f3d070fe7b7a
Joshua Stoutenburg <jstoutenburg@deseretdigital.com>
2013-10-30 21:25:34 -0600

    Adds assertion to BishopTest::testMoves for blocked movement
    + Bishop should have four possible moves
    + Bishop should be able to move to %s

--- tests/Chess/Piece/BishopTest.php
+++ tests/Chess/Piece/BishopTest.php
@@ -2,6 +2,7 @@
 
 use \Chess\Board;
 use \Chess\Piece;
+use \Chess\Piece\Pawn;
 use \Chess\Piece\Bishop;
 
 class BishopTest extends \PHPUnit_Framework_TestCase
@@ -18,6 +19,23 @@ class BishopTest extends \PHPUnit_Framework_TestCase
         $positions = array('A1', 'B2', 'C3', 'E5', 'F6', 'G7', 'H8', 'A7', 'B6', 'C5', 'E3', 'F2', 'G1');
         foreach ($positions as $position) {
             $this->assertContains($position, $moves, sprintf($message, $position));
+        }
+
+        // blocked
+        $board = new Board(array(
+            'D4' => new Bishop(Piece::LIGHT),
+            'C5' => new Pawn(Piece::DARK),
+            'F6' => new Pawn(Piece::DARK),
+            'C3' => new Pawn(Piece::LIGHT),
+            'F2' => new Pawn(Piece::LIGHT),
+        ));
+        $moves = $board->piece('D4')->moves();
+        $this->assertCount(4, $moves, 'Bishop should have four possible moves');
+
+        $message = 'Bishop should be able to move to %s';
+        $positions = array('E5', 'F6', 'C5', 'E3');
+        foreach ($positions as $position) {
+            $this->assertContains($position, $moves, sprintf($message, $positions));
         }
     }
 }
