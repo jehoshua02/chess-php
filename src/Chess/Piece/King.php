678f90e3e69cdc23e69d75789883544151fac9e1
Joshua Stoutenburg <jstoutenburg@deseretdigital.com>
2013-10-31 21:58:09 -0600

    Satisfies KingTest::testMoves

--- src/Chess/Piece/King.php
+++ src/Chess/Piece/King.php
@@ -4,5 +4,48 @@ namespace Chess\Piece;
 
 class King extends \Chess\Piece
 {
+    /**
+     * Returns possible moves for piece
+     * @return array
+     */
+    public function moves()
+    {
+        $moves = array();
 
+        $directions = array(
+            'up', 'down', 'left', 'right',
+            'upLeft', 'upRight', 'downLeft', 'downRight'
+        );
+
+        foreach ($directions as $direction) {
+            $moves[] = $this->step($direction);
+        }
+
+        $moves = array_filter($moves, function ($move) {
+            return $move !== false;
+        });
+
+        return $moves;
+    }
+
+    /**
+     * Returns position in the direction specified
+     * @param  string $direction
+     * @return string|false Returns false if not valid move for piece
+     */
+    protected function step($direction)
+    {
+        $position = $this->board()->$direction($this->position());
+
+        if (!$position) {
+            return false;
+        }
+
+        $piece = $this->board()->piece($position);
+        if ($piece && $piece->color() === $this->color()) {
+            return false;
+        }
+
+        return $position;
+    }
 }
