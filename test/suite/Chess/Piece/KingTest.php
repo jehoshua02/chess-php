2d1843fefb32363c7965a8c576af0a792b468ce4
Joshua Stoutenburg <jstoutenburg@deseretdigital.com>
2013-11-01 23:58:21 -0600

    Moves tests into subdirectory
    I'm planning on adding a PieceTestCase with an assertMoves() method. Need
    to separate from tests into an autoloadable directory within the test
    folder.

--- test/suite/Chess/Piece/KingTest.php
+++ test/suite/Chess/Piece/KingTest.php
@@ -2,6 +2,7 @@
 
 use \Chess\Board;
 use \Chess\Piece;
+use \Chess\Piece\Pawn;
 use \Chess\Piece\King;
 
 class KingTest extends \PHPUnit_Framework_TestCase
@@ -24,6 +25,17 @@ class KingTest extends \PHPUnit_Framework_TestCase
         $moves = $board->piece('A4')->moves();
         $this->assertCount(5, $moves, 'King should have five possible moves');
         foreach (array('A5', 'B5', 'B4', 'A3', 'B3') as $position) {
+            $this->assertContains($position, $moves, sprintf('King should be able to move to %s', $position));
+        }
+
+        // blocked
+        $board = new Board(array(
+            'D4' => new King(Piece::LIGHT),
+            'D5' => new Pawn(Piece::LIGHT)
+        ));
+        $moves = $board->piece('D4')->moves();
+        $this->assertCount(7, $moves, 'King should have seven possible moves');
+        foreach (array('C5', 'E5', 'C4', 'E4', 'C3', 'D3', 'E3') as $position) {
             $this->assertContains($position, $moves, sprintf('King should be able to move to %s', $position));
         }
     }
